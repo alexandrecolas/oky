2248a5bd393fa2ecfa291e225fe3dd9b5428caf6
Alexandre Colas <colasalexandre@gmail.com>
2019-01-09 10:34:15 +0100

    Add validate function documentation

--- src/validate.ts
+++ src/validate.ts
@@ -4,25 +4,39 @@ import validateValidator from "./validate-validator";
 import validateValidators from "./validate-validators";
 import validateHash from "./validate-hash";
 
-const isSchemaHash = function(schema: any): schema is object {
-  return isHash(schema);
+const isValidatorsHash = function(validators: any): validators is object {
+  return isHash(validators);
 };
 
-const isSchemaValidators = function(schema: any): schema is Function[] {
-  return is(Array, schema);
+const isValidatorsValidators = function(
+  validators: any
+): validators is Function[] {
+  return is(Array, validators);
 };
 
-const isSchemaValidator = function(schema: any): schema is Function {
-  return is(Function, schema);
+const isValidatorsValidator = function(
+  validators: any
+): validators is Function {
+  return is(Function, validators);
 };
 
 /**
  * Validate
+ * @param {(Function |Function[] | object)} validators
+ * @param {any} value - value to test
+ * @returns {(boolean | string | string[] | object)} Return true when success or errors when failure.
+ *    Errors format depends on the validators format :
+ *    it return an array if validators args is an array and object if validators args is an object
  */
-const validate = function(schema: object | Function[] | Function, value: any) {
-  if (isSchemaHash(schema)) return validateHash(schema, value);
-  if (isSchemaValidators(schema)) return validateValidators(schema, value);
-  if (isSchemaValidator(schema)) return validateValidator(schema, value);
+const validate = function(
+  validators: Function | Function[] | object,
+  value: any
+): boolean | string | string[] | object {
+  if (isValidatorsHash(validators)) return validateHash(validators, value);
+  if (isValidatorsValidators(validators))
+    return validateValidators(validators, value);
+  if (isValidatorsValidator(validators))
+    return validateValidator(validators, value);
 };
 
 export default curry(validate);
